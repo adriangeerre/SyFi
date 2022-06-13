bb8bc6fd2e79c3f2a96643b3f4dff9ecce0fe4e2
adriangeerre <adriangomezrepolles@gmail.com>
2022-06-13 12:08:35 +0200

    filter_haplotype

---
+++ filterHaplotypes.R
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+#----------------------------------------------------------------------------
+# Created By  	: Adrián Gómez Repollés
+# Email			    : adrian.gomez@mbg.au.dk
+# Created Date	: 13/06/2022
+# version 		  : '1.0'
+# ---------------------------------------------------------------------------
+# """ Script to compute the haplotype ratio from the Kallisto output. The ra-
+# tio will be used to filter out unwanted haplotypes.""" 
+# ---------------------------------------------------------------------------
+
+# Libraries
+# ---------
+library(optparse)
+
+# Parameters
+# ----------
+option_list = list(
+  make_option(c("-i", "--input"), action="store", default=NA, type='character',
+              help="Input path with kallisto output file."),
+  )
+opt = parse_args(OptionParser(option_list=option_list))
+
+# Data
+# ----
+df <- read.delim(opt$i, header=T)
+
+# Transform
+# --------
+while(TRUE) {
+  minimum <- min(df$est_counts)
+  v <- df$est_counts / minimum
+  if (max(v) > 25) {
+    df <- df[df$est_counts != minimum,]
+  } else {
+    # Ratio
+    df$ratio <- df$est_counts / minimum
+    break
+  }
+}
+
+# Save
+# ----
+write.table(df, file=opt$i, quote=F, col.names=T, row.names=F, sep="\t")
