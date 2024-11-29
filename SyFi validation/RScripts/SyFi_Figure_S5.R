f0560ebce6782c3d93e2efa1b70354bb3974f998
Gijsselten13 <117294807+Gijsselten13@users.noreply.github.com>
2024-11-29 12:02:53 +0100

    Create SyFi_Figure_S5.R

---
+++ SyFi validation/RScripts/SyFi_Figure_S5.R
@@ -0,0 +1,177 @@
+#Required R packages
+library("ggplot2") #Version 3.4.2
+library("reshape2") #Version 1.4.4
+library("ggpubr") #Version 0.6.0
+library("ggrepel") #Version 0.9.3
+library("tidyheatmaps") #Version 0.1.0
+library("grid") #Version 4.4.2
+library("patchwork") #Version 1.1.2
+
+#Set working_directory and results directory
+working_directory <- ""
+dir.create(paste(working_directory, "results", sep = ""))
+results.dir <- paste(working_directory, "results/", sep="")
+
+###Figure S5 - SyFi Accuracy with different Salmon minscorefraction settings=====
+
+Hank_the_normalizer <- function(df,group,amount){
+  df_2 <- df %>% dplyr::group_by_at(group) %>% dplyr::summarise(total=sum(.data[[amount]]))
+  df_3 <- df_2$total
+  names(df_3) <- df_2[[group]]
+  df$total <- df_3[as.character(df[[group]])]
+  df$Rel <- df[[amount]] / df$total
+  return(df)
+}
+vector_2 <- c("65_V3V4", "75_V3V4", "85_V3V4", "95_V3V4", "100_V3V4", "65_V5V7", "75_V5V7", "85_V5V7", "95_V5V7", "100_V5V7")
+
+correlations_min <- as.data.frame(vector_2)
+correlations_min$Correlation <- NA
+
+for (metric in vector_2) {
+  #Insert metagenome table
+  otu_meta = read.table(paste(working_directory,"minscorefraction/AtSC_meta_norm.tsv", sep =""), header=T, sep="\t")
+  
+  row.names(otu_meta) <- otu_meta$isolate
+  otu_meta <- otu_meta %>% dplyr::select(-isolate)
+  
+  #Insert table for comparison
+  otu_table = read.table(paste0(working_directory,"minscorefraction/", metric, ".tsv",sep =""), header=T, sep="\t")
+  
+  row.names(otu_table) <- otu_table$isolate
+  otu_table <- otu_table %>% dplyr::select(-isolate)
+  
+  #Insert metadata file
+  samples_meta = read.table(paste(working_directory,"metadata.txt", sep = ""), header=TRUE,sep="\t")
+  
+  row.names(samples_meta) <- samples_meta$sample_id
+  samples_meta <- samples_meta %>% dplyr::select(-sample_id)
+  
+  #Insert clusters file
+  clusters = read.csv(paste0(working_directory,"vsearch_clusters_minscorefraction/collapse_",metric, ".txt",sep=""), header=F, sep="\t")
+  
+  #Reformatting table of interest 
+  no_of_isolates <- length(row.names(clusters))
+  
+  #Create new table with the right dimensions
+  new_table <- data.frame(matrix(NA, nrow = no_of_isolates, ncol = length(row.names(samples_meta))+1))
+  
+  for (number in 1:no_of_isolates){
+    new_table[number,1] <- paste("Cluster_", number,sep="")
+  }
+  
+  column_names <- c("cluster", paste(row.names(samples_meta),sep=""))
+  colnames(new_table) <- column_names
+  
+  row.names(new_table) <- new_table$cluster
+  new_table <- new_table %>% dplyr::select(-cluster)
+  
+  #Reorder column names of otu_table
+  colnames(otu_table) = gsub("_t3", "", colnames(otu_table))
+  colnames(otu_table) = gsub("_t0", "", colnames(otu_table))
+  colnames(otu_table) = gsub("_undil", "", colnames(otu_table))
+  colnames(otu_table) = gsub("_v5_v7", "", colnames(otu_table))
+  colnames(otu_table) = gsub("_v3_v4", "", colnames(otu_table))
+  column_names_order <- c(paste(row.names(samples_meta),sep=""))
+  otu_table_2 <- otu_table[column_names_order]
+  otu_table <- otu_table_2
+  
+  #Merge values from isolates of the same cluster - table for comparison
+  for (row in 1:nrow(clusters)){
+    isolate_vector <- as.character(clusters[row,])
+    isolate_vector <- isolate_vector[!is.na(isolate_vector)]
+    isolate_vector <- isolate_vector[!isolate_vector == ""]
+    
+    if (length(isolate_vector) > 1){
+      other_list <- row.names(otu_table) 
+      the_isolate <- isolate_vector[isolate_vector %in% other_list]
+    } else {
+      the_isolate <- isolate_vector
+    }
+    
+    #subset otu_table of choice
+    otu_table_2 <- otu_table[row.names(otu_table) %in% paste0(the_isolate),]
+    otu_table_3 <- colSums(otu_table_2)
+    
+    new_table[row,] <- otu_table_3
+  }
+  
+  #Create new table with the right dimensions for metagenome table
+  meta_table <- data.frame(matrix(NA, nrow = no_of_isolates, ncol = length(row.names(samples_meta))+1))
+  
+  for (number in 1:no_of_isolates){
+    meta_table[number,1] <- paste("Cluster_", number,sep="")
+  }
+  
+  column_names <- c("cluster", paste(row.names(samples_meta),sep=""))
+  colnames(meta_table) <- column_names
+  
+  row.names(meta_table) <- meta_table$cluster
+  meta_table <- meta_table %>% dplyr::select(-cluster)
+  
+  colnames(otu_meta) = gsub("_undil", "", colnames(otu_meta))
+  column_names_order <- c(paste(row.names(samples_meta),sep=""))
+  otu_meta_2 <- otu_meta[column_names_order]
+  otu_meta <- otu_meta_2
+  
+  #Merge values from isolates of the same cluster - table for comparison
+  for (row in 1:nrow(clusters)){
+    isolate_vector <- as.character(clusters[row,])
+    other_list <- row.names(otu_meta) 
+    the_isolate <- isolate_vector[isolate_vector %in% other_list]
+    the_isolate <- the_isolate[!is.na(the_isolate)]
+    the_isolate <- the_isolate[!the_isolate == ""]
+    
+    #subset otu_table of choice
+    otu_table_2 <- otu_meta[row.names(otu_meta) %in% paste0(the_isolate),]
+    otu_table_3 <- colSums(otu_table_2)
+    
+    meta_table[row,] <- otu_table_3
+  }
+  
+  #Melt tables
+  meta_table$Cluster <- row.names(meta_table)
+  meta_table_2 <- melt(meta_table)
+  new_table$Cluster <- row.names(new_table)
+  new_table_2 <- melt(new_table)
+  length(new_table[new_table[,2] > 0,2])
+  #Make relative abundances
+  meta_table_3 <- Hank_the_normalizer(meta_table_2,"variable","value")
+  new_table_3 <- Hank_the_normalizer(new_table_2,"variable","value")
+  
+  meta_table_3$ID <- paste(meta_table_3$variable,meta_table_3$Cluster,sep="_")
+  new_table_3$ID <- paste(new_table_3$variable,new_table_3$Cluster,sep="_")
+  
+  fused_table <- data.frame(matrix(NA, nrow = length(meta_table_3$ID), ncol = 0))
+  fused_table$V1 <- meta_table_3$Rel
+  row.names(fused_table) <- meta_table_3$ID
+  fused_table$V2 <- new_table_3$Rel[match(row.names(fused_table),new_table_3$ID)]
+  colnames(fused_table) <- c("metagenome_abundances", "metric_abundances")
+  
+  fused_table_2 <- na.omit(fused_table[fused_table$metric_abundances != 0,])
+  
+  value <- cor(fused_table_2$metagenome_abundances,fused_table_2$metric_abundances)
+  correlations_min$Correlation[correlations_min$vector_2 == paste0(metric)] <- value
+}
+
+correlations_min$Metric <- c("V3-V4", "V3-V4", "V3-V4", "V3-V4","V3-V4", "V5-V7", "V5-V7", "V5-V7", "V5-V7", "V5-V7")
+correlations_min$percentage <- correlations_min$vector_2
+correlations_min$percentage <-  gsub("_....*","",correlations_min$percentage)
+
+correlations_min_2 <- correlations_min[correlations_min$percentage != "100",]
+
+plot_line <- ggplot(correlations_min_2, aes(x = percentage, y = Correlation, group = Metric)) + 
+  geom_line(aes(color = Metric),size=1.2) +
+  geom_point(aes(color = Metric)) + 
+  ggtitle("SyFi with different pseudoalignment parameters") + 
+  theme(plot.title = element_text(hjust = 0.5)) + 
+  ylab("Correlation") + 
+  xlab("Percentage identity pseudoalignment") +
+  theme(axis.text.x = element_text(size = 14), axis.title = element_text(size = 18), axis.text.y = element_text(size=14), legend.title = element_text(size=18), legend.text = element_text(size=14), plot.title = element_text(size=24)) +
+  theme(panel.border = element_blank(),panel.grid.major = element_blank(),panel.grid.minor = element_blank(),panel.background = element_blank(),axis.line = element_line(colour = "black"))
+plot_line
+
+pdf(paste(results.dir,"Figure_S5.pdf", sep=""), width=8, height=6)
+print(plot_line)
+dev.off()
+
+
